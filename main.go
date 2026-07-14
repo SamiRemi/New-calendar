9440e24dbc5c28242c5de520fd640b9deeab2323
SamiRemi <Pofacty>
2026-07-14 21:54:53 +0500

    test

--- main.go
+++ main.go
@@ -28,6 +28,12 @@ func main() {
 	calendar.AddEvent("event2", e1)
 	calendar.AddEvent("event3", e2)
 	calendar.DeleteEvent("event2")
+	fmt.Println("hello")
+	fmt.Println("hello")
+	fmt.Println("hello")
+	fmt.Println("hello")
+	fmt.Println("hello")
+
 	err3 := calendar.EditEvent("event11", "Meet", "2025/07/25 20:35")
 	if err3 != nil {
 		fmt.Println("Ошибка:", err3)
